ccfecd009ee3cd4eb9e364ab9037c9bdcacc4cc9
blessing <blessingmike334@gmail.com>
2025-01-23 18:37:41 +0100

    Add test for basic auth

--- cypress/e2e/ab-test.cy.js
+++ cypress/e2e/ab-test.cy.js
@@ -5,37 +5,37 @@ describe("Add/Remove Elements Page Tests", () => {
   beforeEach(() => {
     // Navigate to the A/B Test Variation 1 page before each test
     cy.visit(baseUrl);
-});
-it("verify page title", () => {
+  });
+  // Ensure the correct page title is displayed
+  it("verify page title", () => {
     cy.title().should("eq", "The Internet");
   });
-  
-  it('it should have correct page header', ()=>{
-    cy.get('h3').should('contain.text', 'A/B Test Control')
-})
-  it('it should ensure paragraph contains text', ()=>{
-    cy.get('p').should('not.be.empty')
-  })
- 
-  it('should display the footer link to Elemental Selenium', () => {
-    cy.get('div#page-footer a')
-      .should('be.visible')
-      .and('have.text', 'Elemental Selenium')
-      .and('have.attr', 'href', 'http://elementalselenium.com/');
+  // Ensure correct page header is displayed
+  it("it should have correct page header", () => {
+    cy.get("h3").should("contain.text", "A/B Test Control");
   });
-  it('it should verify footer link', ()=>{
-    cy.get('div#page-footer a')
-    .should('be.visible')
-    .and('have.text', 'Elemental Selenium')
-    .and('have.attr', 'href', 'http://elementalselenium.com/')
-  })
-  it('should verify that the variation header matches expected options', () => {
-    const expectedHeaders = ['A/B Test Control', 'A/B Test Variation 1'];
-    cy.get('h3').then(($header) => {
+  // Ensure the paragraph contains text
+  it("it should ensure paragraph contains text", () => {
+    cy.get("p").should("not.be.empty");
+  });
+
+  it("should display the footer link to Elemental Selenium", () => {
+    cy.get("div#page-footer a")
+      .should("be.visible")
+      .and("have.text", "Elemental Selenium")
+      .and("have.attr", "href", "http://elementalselenium.com/");
+  });
+  it("it should verify footer link", () => {
+    cy.get("div#page-footer a")
+      .should("be.visible")
+      .and("have.text", "Elemental Selenium")
+      .and("have.attr", "href", "http://elementalselenium.com/");
+  });
+  it("should verify that the variation header matches expected options", () => {
+    const expectedHeaders = ["A/B Test Control", "A/B Test Variation 1"];
+    cy.get("h3").then(($header) => {
       const headerText = $header.text();
       expect(expectedHeaders).to.include(headerText);
     });
   });
-
 });
-
