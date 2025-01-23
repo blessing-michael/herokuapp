9febb47eec50930a6a309eb2b8770da48d2c7eba
blessing <blessingmike334@gmail.com>
2025-01-23 18:18:43 +0100

    Add test to verify header variation text

--- cypress/e2e/ab-test.cy.js
+++ cypress/e2e/ab-test.cy.js
@@ -16,6 +16,26 @@ it("verify page title", () => {
   it('it should ensure paragraph contains text', ()=>{
     cy.get('p').should('not.be.empty')
   })
+ 
+  it('should display the footer link to Elemental Selenium', () => {
+    cy.get('div#page-footer a')
+      .should('be.visible')
+      .and('have.text', 'Elemental Selenium')
+      .and('have.attr', 'href', 'http://elementalselenium.com/');
+  });
+  it('it should verify footer link', ()=>{
+    cy.get('div#page-footer a')
+    .should('be.visible')
+    .and('have.text', 'Elemental Selenium')
+    .and('have.attr', 'href', 'http://elementalselenium.com/')
+  })
+  it('should verify that the variation header matches expected options', () => {
+    const expectedHeaders = ['A/B Test Control', 'A/B Test Variation 1'];
+    cy.get('h3').then(($header) => {
+      const headerText = $header.text();
+      expect(expectedHeaders).to.include(headerText);
+    });
+  });
 
 });
 
