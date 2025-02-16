6ea10603fbe1d25cb6dc791ea06b06ebee9ad68a
blessing <blessingmike334@gmail.com>
2025-02-16 20:08:04 +0100

    Add test for challenging dom page

---
+++ cypress/e2e/challenging-dom.cy.js
@@ -0,0 +1,23 @@
+/// <reference types = "cypress" />
+describe("Test case for challenging dom page", () => {
+  const baseUrl = "https://the-internet.herokuapp.com/challenging_dom";
+  beforeEach(() => {
+    cy.visit(baseUrl);
+  });
+
+  it("should verify the correct title", () => {
+    cy.title().should("eq", "The Internet");
+  });
+  it("ensures elements are in the correct order", function () {
+    // Verify that elements are ordered correctly
+    cy.get(".example").find("h3").eq(0).should("have.text", "Challenging DOM");
+  });
+  it('verifies dynamic table contents', function () {
+    // Verify that the dynamic table is populated with data
+    // ensures that there are multiple rows in the table
+    cy.get('table').find('tr').should('have.length.greaterThan', 1); 
+
+    // Verify that a row contains a specific text
+    cy.get('table tr').contains('1').should('exist');
+  });
+});
