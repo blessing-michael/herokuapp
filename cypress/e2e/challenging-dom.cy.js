e2126844a1dd052910b96baa20c3bf6d76bbee25
blessing <blessingmike334@gmail.com>
2025-02-17 11:06:56 +0100

    Fix error in challenging dom

--- cypress/e2e/challenging-dom.cy.js
+++ cypress/e2e/challenging-dom.cy.js
@@ -1,12 +1,8 @@
 /// <reference types = "cypress" />
 describe("Test case for challenging dom page", () => {
   const baseUrl = "https://the-internet.herokuapp.com/challenging_dom";
-  const possibleTexts = [
-    'bar',
-    'baz',
-    'qux',
-    'foo'
-  ];
+  const possibleTexts = ["bar", "baz", "qux", "foo"];
+  const buttonNames = ["button", "alert", "success"];
   beforeEach(() => {
     cy.visit(baseUrl);
   });
@@ -18,28 +14,19 @@ describe("Test case for challenging dom page", () => {
     // Verify that elements are ordered correctly
     cy.get(".example").find("h3").eq(0).should("have.text", "Challenging DOM");
   });
-  it('verifies dynamic table contents', function () {
+  it("verifies dynamic table contents", function () {
     // Verify that the dynamic table is populated with data
     // ensures that there are multiple rows in the table
-    cy.get('table').find('tr').should('have.length.greaterThan', 1); 
+    cy.get("table").find("tr").should("have.length.greaterThan", 1);
 
     // Verify that a row contains a specific text
-    cy.get('table tr').contains('1').should('exist');
+    cy.get("table tr").contains("1").should("exist");
   });
 
-  it('clicks all the buttons serially', function () {
-   
-    
-   
-    cy.get('.button').each(($btn, index) => {
+  it("clicks all the buttons serially", function () {
+    cy.get(".alert").each(($btn, index) => {
       // For each button, log the text and click it
-      cy.wrap($btn)
-      .should('be.visible')
-      
-      
-      
+      cy.wrap($btn).should("be.visible").click();
     });
-      
-  
   });
 });
